9e9e5f42221d15d53272dccb1db88c216b73f1f5
omgninjas <omgninjas@me.com>
2018-07-26 10:16:08 -0400

    update repo with July challenges

---
+++ basic-data-structures/iteratekeys.js
@@ -0,0 +1,28 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function countOnline(obj) {
+  // change code below this line
+  let count = 0;
+  for(let user in obj) if (obj[user].online) count++
+  return count;
+  // change code above this line
+};
+
+console.log(countOnline(users));
